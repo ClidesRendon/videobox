246eb85d8203ef3d431b360da494a017905b963a
Clides <clides.rendon.pons@protonmail.com>
2025-07-11 13:20:16 +0200

    Added TRCP Config

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -4,6 +4,8 @@ import { users } from '@/db/schema';
 import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError} from '@trpc/server';
 import { cache } from 'react';
+import superjson from 'superjson';
+import { ratelimit } from '@/lib/ratelimit';
 
 export const createTRPCContext = cache(async () => {
     const { userId } = await auth();
@@ -28,3 +30,41 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+
+//CHECK IF USER IS AUTHED
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
+    const { ctx } = opts;
+
+    //IF IS NOT "CLERKED"
+    if (!ctx.clerkUserId){
+        throw new TRPCError( {code: "UNAUTHORIZED"});
+    }
+
+    //IF IT'S CLERKED, BUT NOT ON DB
+    const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, ctx.clerkUserId))
+        .limit(1)
+
+    if (!user){
+        throw new TRPCError({ code: "UNAUTHORIZED"})
+    }
+
+    //CHECK FOR RATELIMIT
+    const { success } = await ratelimit.limit(user.id)
+
+    if(!success) {
+        throw new TRPCError({code: "TOO_MANY_REQUESTS"})
+    }
+
+
+    return opts.next({
+        ctx: {
+            ...ctx,
+            user
+        }
+    })
+    
+});
