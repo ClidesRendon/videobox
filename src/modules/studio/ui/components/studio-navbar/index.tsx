6d4cb0b8bce4c9c10037ccae8ea7e246a24c2606
Clides <clides.rendon.pons@protonmail.com>
2025-07-16 15:23:01 +0200

    Studio Layout Done

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -2,10 +2,12 @@ import { SidebarTrigger } from "@/components/ui/sidebar"
 import Link from "next/link"
 import Image from "next/image"
 import { AuthButton } from "@/modules/auth/ui/components/auth-button"
+import { StudioUploadModal } from "../../studio-upload-modal"
 
 export const StudioNavbar = () => {
     return (
-        <nav className="fixed top-0 left-0 right-0 h-15 bg-white flex items-center px-5 pr-3 z-50">
+        <nav className="fixed top-0 left-0 right-0 h-15 bg-white flex items-center px-5 pr-3 z-50
+        border-b shadow-md">
 
             
             <div className="flex items-center gap-4 w-full">
@@ -25,6 +27,7 @@ export const StudioNavbar = () => {
 
                 {/*SIGN UP-IN BUTTON */}
                 <div className="flex-shrink-0 items-center flex gap-4">
+                    <StudioUploadModal />
                     <AuthButton />
 
                 </div>
